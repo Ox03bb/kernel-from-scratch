e8749de7f7a713099f31a9e8d965cf8db2545c3d
Ox03bb <ali.de.bouakaz@gmail.com>
2026-09-13 01:50:31 +0100

    init the ps2.h file

---
+++ includes/driver/ps2.h
@@ -0,0 +1,48 @@
+#ifndef PS2_H
+#define PS2_H
+
+#include <stdint.h>
+
+#define PS2_DATA_PORT 0x60
+#define PS2_STATUS_PORT 0x64
+
+// control commands
+#define PS2_CC_READ_CONFIG 0x20 // read configuration byte
+#define PS2_CC_WRITE_CONFIG 0x60 // write configuration byte
+#define PS2_CC_SELF_TEST 0xAA // controller self-test
+#define PS2_CC_TEST_PORT1 0xAB // test port 1
+#define PS2_CC_TEST_PORT2 0xA9 // test port 2
+#define PS2_CC_DISABLE_PORT1 0xAD // disable port 1
+#define PS2_CC_ENABLE_PORT1 0xAE // enable port 1
+#define PS2_CC_DISABLE_PORT2 0xA7 // disable port 2
+#define PS2_CC_ENABLE_PORT2 0xA8 // enable port 2
+#define PS2_CC_READ_OUTPUT_PORT 0xD0 // read output port
+#define PS2_CC_WRITE_OUTPUT_PORT 0xD1 // write output port
+#define PS2_CC_WRITE_TO_PORT2 0xD4 // next byte goes to port 2
+
+// device commands
+#define PS2_DC_RESET 0xFF // reset device
+#define PS2_DC_DISABLE_SCANNING 0xF5 // disable scanning
+#define PS2_DC_IDENTIFY_DEVICE 0xF2 // identify device
+#define PS2_DC_ENABLE_SCANNING 0xF4 // enable scanning
+
+struct ps2_config {
+    uint8_t port1_interrupt : 1;
+    uint8_t port2_interrupt : 1;
+    uint8_t system_flag : 1;
+    uint8_t zero1 : 1;
+    uint8_t port1_clock : 1;
+    uint8_t port2_clock : 1;
+    uint8_t _ : 2;
+};
+
+// ps2_read();
+// ps2_write();
+// ps2_wait_input();
+// ps2_wait_output();
+// ps2_send_device_command();
+// ps2_read_config();
+// ps2_write_config();
+// ps2_status();
+
+#endif
