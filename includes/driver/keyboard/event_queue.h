ef9c349b3ea82de2193a775807044a84a36eeb35
Ox03bb <ali.de.bouakaz@gmail.com>
2026-09-21 19:47:29 +0100

    add event queue ofor keyboard

---
+++ includes/driver/keyboard/event_queue.h
@@ -0,0 +1,25 @@
+#ifndef EVENT_QUEUE_H
+#define EVENT_QUEUE_H
+
+#include "keyboard_keys.h"
+#include "keyboard.h"
+#include "ps2.h"
+#include "types.h"
+
+
+#define KEYBOARD_EVENT_QUEUE_SIZE 256
+
+typedef struct {
+    key_event_t events[KEYBOARD_EVENT_QUEUE_SIZE];
+    uint32_t head;
+    uint32_t tail;
+} keyboard_event_queue_t;
+
+extern keyboard_event_queue_t keyboard_queue;
+
+bool keyboard_queue_push(key_event_t event);
+bool keyboard_queue_pop(key_event_t *event);
+
+void keyboard_queue_print();
+
+#endif
---
+++ src/driver/keyboard/event_queue.c
@@ -0,0 +1,45 @@
+#include "event_queue.h"
+#include "stdio.h"
+
+keyboard_event_queue_t keyboard_queue = {0};
+
+
+bool keyboard_queue_push(key_event_t event)
+{
+    uint32_t next =
+        (keyboard_queue.head + 1) % KEYBOARD_EVENT_QUEUE_SIZE;
+
+    if (next == keyboard_queue.tail) {
+        return false; /* queue full */
+    }
+
+    keyboard_queue.events[keyboard_queue.head] = event;
+    keyboard_queue.head = next;
+
+    return true;
+}
+
+bool keyboard_queue_pop(key_event_t *event)
+{
+    if (keyboard_queue.head == keyboard_queue.tail) {
+        return false; /* queue empty */
+    }
+
+    *event = keyboard_queue.events[keyboard_queue.tail];
+
+    keyboard_queue.tail =
+        (keyboard_queue.tail + 1) % KEYBOARD_EVENT_QUEUE_SIZE;
+
+    return true;
+}
+
+
+void keyboard_queue_print() {
+
+
+    printf("\033[32mKeyboard Queue: \033[0m head=%d, tail=%d\n", keyboard_queue.head, keyboard_queue.tail);
+    for (uint32_t i = 0; i < keyboard_queue.head - keyboard_queue.tail; i++) {
+        key_event_t event = keyboard_queue.events[i];
+        printf("Event[%d]: key=%d, action=%d\n", i, event.key, event.action);
+    }
+}
