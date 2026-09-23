a4dabdb84bf14226094ee2cac61786b6431eff83
Ox03bb <ali.de.bouakaz@gmail.com>
2026-09-23 16:42:16 +0100

    refactor(tty): cahgen the old stracture  and add a console attr to tty

---
+++ src/driver/tty.c
@@ -0,0 +1,236 @@
+#include "tty.h"
+#include "driver/vga.h"
+#include "keyboard/event_queue.h"
+#include "keyboard_keys.h"
+
+#include "console.h"
+
+void tty_putc(tty_t *tty, char c);
+void tty_write(tty_t *tty, const char *str);
+void tty_read(tty_t *tty, char *buf, size_t len);
+void tty_clear(tty_t *tty);
+
+static const tty_interface_t tty_default_interface = {
+    .putc = tty_putc,
+    .write = tty_write,
+    .read = tty_read,
+    .clear = tty_clear,
+};
+
+void tty_init(tty_t *tty) {
+
+    if (tty == NULL)
+        return;
+
+    tty->echo = true;
+    tty->line_ready = false;
+
+    tty->input_length = 0;
+    tty->buffer_index = 0;
+
+    tty->modifiers = MOD_NONE;
+
+    for (size_t i = 0; i < TTY_BUFFER_SIZE; i++)
+        tty->buffer[i] = '\0';
+
+    tty->method = tty_default_interface;
+
+    console_init(tty->console);
+}
+
+void tty_putc(tty_t *tty, char c) {
+    if (tty == NULL)
+        return;
+
+    if (tty->buffer_index >= TTY_BUFFER_SIZE - 1)
+        return;
+
+    tty->buffer[tty->buffer_index++] = c;
+    tty->buffer[tty->buffer_index] = '\0';
+
+    tty->input_length++;
+}
+
+void tty_write(tty_t *tty, const char *str) {
+    if (tty == NULL || str == NULL)
+        return;
+
+    for (size_t i = 0; str[i] != '\0'; i++)
+        tty_putc(tty, str[i]);
+}
+
+void tty_read(tty_t *tty, char *buf, size_t len) {
+    if (tty == NULL || buf == NULL || len == 0)
+        return;
+
+    size_t i = 0;
+
+    /*
+     * Reserve one byte for '\0'.
+     */
+    while (i < len - 1 && tty->buffer[i] != '\0') {
+        buf[i] = tty->buffer[i];
+        i++;
+    }
+
+    buf[i] = '\0';
+}
+
+void tty_clear(tty_t *tty) {
+    if (tty == NULL)
+        return;
+
+    for (size_t i = 0; i < TTY_BUFFER_SIZE; i++)
+        tty->buffer[i] = '\0';
+
+    tty->buffer_index = 0;
+    tty->input_length = 0;
+    tty->line_ready = false;
+}
+
+void tty_update_modifiers(tty_t *tty, const input_event_t *event) {
+    if (tty == NULL || event == NULL)
+        return;
+
+    bool pressed = event->action == KEY_PRESSED;
+
+    switch (event->key) {
+
+    case KEY_LEFT_SHIFT:
+    case KEY_RIGHT_SHIFT:
+        if (pressed)
+            tty->modifiers |= MOD_SHIFT;
+        else
+            tty->modifiers &= ~MOD_SHIFT;
+        break;
+
+    case KEY_LEFT_CTRL:
+    case KEY_RIGHT_CTRL:
+        if (pressed)
+            tty->modifiers |= MOD_CTRL;
+        else
+            tty->modifiers &= ~MOD_CTRL;
+        break;
+
+    case KEY_LEFT_ALT:
+        if (pressed)
+            tty->modifiers |= MOD_ALT;
+        else
+            tty->modifiers &= ~MOD_ALT;
+        break;
+
+    case KEY_RIGHT_ALT:
+        if (pressed)
+            tty->modifiers |= MOD_ALTGR;
+        else
+            tty->modifiers &= ~MOD_ALTGR;
+        break;
+
+    case KEY_CAPS_LOCK:
+        if (pressed)
+            tty->modifiers ^= MOD_CAPS;
+        break;
+
+    case KEY_NUM_LOCK:
+        if (pressed)
+            tty->modifiers ^= MOD_NUM;
+        break;
+
+    case KEY_SCROLL_LOCK:
+        if (pressed)
+            tty->modifiers ^= MOD_SCROLL;
+        break;
+
+    default:
+        break;
+    }
+}
+
+void tty_handle_event(tty_t *tty, const input_event_t *event) {
+    if (tty == NULL || event == NULL)
+        return;
+
+    tty_update_modifiers(tty, event);
+
+    if (event->action != KEY_PRESSED)
+        return;
+
+    switch (event->key) {
+
+    case KEY_LEFT_SHIFT:
+    case KEY_RIGHT_SHIFT:
+
+    case KEY_LEFT_CTRL:
+    case KEY_RIGHT_CTRL:
+
+    case KEY_LEFT_ALT:
+    case KEY_RIGHT_ALT:
+
+    case KEY_CAPS_LOCK:
+    case KEY_NUM_LOCK:
+    case KEY_SCROLL_LOCK:
+
+        return;
+
+    default:
+        break;
+    }
+
+    if (tty->modifiers & MOD_CTRL) {
+
+        switch (event->key) {
+
+        case KEY_C:
+            /*
+             * Ctrl+C
+             *
+             * Process/signal handling can be added later.
+             */
+            return;
+
+        default:
+            break;
+        }
+    }
+
+    switch (event->key) {
+
+    case KEY_BACKSPACE:
+
+        if (tty->buffer_index > 0) {
+
+            tty->buffer_index--;
+
+            if (tty->input_length > 0)
+                tty->input_length--;
+
+            tty->buffer[tty->buffer_index] = '\0';
+        }
+
+        return;
+
+    case KEY_ENTER:
+
+        if (tty->buffer_index < TTY_BUFFER_SIZE - 1) {
+
+            tty->buffer[tty->buffer_index++] = '\n';
+            tty->buffer[tty->buffer_index] = '\0';
+
+            tty->input_length++;
+
+            tty->line_ready = true;
+        }
+
+        return;
+
+    default:
+        break;
+    }
+
+    char c = event->character;
+
+    if (c == '\0')
+        return;
+
+    tty_putc(tty, c);
+}
