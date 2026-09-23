6510fb7ec53e627f15b3e3f49ffc27361711354c
Ox03bb <ali.de.bouakaz@gmail.com>
2026-09-23 20:55:56 +0100

    feat: add scanf & scan char

--- includes/lib/stdio.h
+++ includes/lib/stdio.h
@@ -12,6 +12,8 @@ void print_at_end(const char *str, uint8_t color);
 
 void printf(const char *format, ...);
 void snprintf(char *buffer, size_t buffer_size, const char *format, ...);
+char scan_char(void);
+int scanf(const char *format, ...);
 
 char *vformatter(const char *format, va_list args);
 char *formatter(const char *format, ...);
--- src/libs/stdio.c
+++ src/libs/stdio.c
@@ -5,11 +5,203 @@
 #include "vga_lib.h"
 
 #include "string.h"
+#include "keyboard/event_queue.h"
 
 #include "stdio.h"
 
 #define FORMATTER_BUFFER_SIZE 1024
 static char buffer[FORMATTER_BUFFER_SIZE];
+static bool scan_has_pending;
+static char scan_pending;
+
+static char scan_read_char(void) {
+    input_event_t event;
+
+    if (scan_has_pending) {
+        scan_has_pending = false;
+        return scan_pending;
+    }
+
+    while (input_queue_pop(&event)) {
+        if (event.action != KEY_PRESSED)
+            continue;
+
+        if (event.type == INPUT_EVENT_CHAR)
+            return event.character;
+
+        if (event.key == KEY_ENTER)
+            return '\n';
+    }
+
+    return '\0';
+}
+
+static void scan_unread_char(char character) {
+    scan_pending = character;
+    scan_has_pending = true;
+}
+
+static bool scan_is_space(char character) {
+    return character == ' ' || character == '\t' || character == '\n' || character == '\r';
+}
+
+char scan_char(void) {
+    char character;
+
+    do {
+        character = scan_read_char();
+    } while (character == '\0');
+
+    return character;
+}
+
+int scanf(const char *format, ...) {
+    va_list args;
+    int assigned = 0;
+
+    va_start(args, format);
+
+    while (*format) {
+        if (scan_is_space(*format)) {
+            while (scan_is_space(*format))
+                format++;
+
+            char character;
+            do {
+                character = scan_char();
+            } while (scan_is_space(character));
+            scan_unread_char(character);
+            continue;
+        }
+
+        if (*format != '%') {
+            char character = scan_char();
+            if (character != *format) {
+                scan_unread_char(character);
+                break;
+            }
+            format++;
+            continue;
+        }
+
+        format++;
+        if (*format == '\0')
+            break;
+
+        switch (*format) {
+        case 'c': {
+            char *value = va_arg(args, char *);
+            *value = scan_char();
+            assigned++;
+            break;
+        }
+
+        case 's': {
+            char *value = va_arg(args, char *);
+            char character;
+            size_t length = 0;
+
+            do {
+                character = scan_char();
+            } while (scan_is_space(character));
+
+            while (!scan_is_space(character)) {
+                value[length++] = character;
+                character = scan_char();
+            }
+            value[length] = '\0';
+            assigned++;
+            break;
+        }
+
+        case 'd': {
+            int *value = va_arg(args, int *);
+            char character;
+            int sign = 1;
+            int number = 0;
+
+            do {
+                character = scan_char();
+            } while (scan_is_space(character));
+
+            if (character == '-' || character == '+') {
+                sign = character == '-' ? -1 : 1;
+                character = scan_char();
+            }
+
+            if (character < '0' || character > '9') {
+                scan_unread_char(character);
+                break;
+            }
+
+            do {
+                number = number * 10 + (character - '0');
+                character = scan_char();
+            } while (character >= '0' && character <= '9');
+
+            scan_unread_char(character);
+            *value = number * sign;
+            assigned++;
+            break;
+        }
+
+        case 'x': {
+            uint32_t *value = va_arg(args, uint32_t *);
+            char character;
+            uint32_t number = 0;
+            bool has_digit = false;
+
+            do {
+                character = scan_char();
+            } while (scan_is_space(character));
+
+            if (character == '0') {
+                char prefix = scan_char();
+                if (prefix == 'x' || prefix == 'X')
+                    character = scan_char();
+                else
+                    scan_unread_char(prefix);
+            }
+
+            while ((character >= '0' && character <= '9') ||
+                   (character >= 'a' && character <= 'f') ||
+                   (character >= 'A' && character <= 'F')) {
+                uint32_t digit;
+                if (character >= '0' && character <= '9')
+                    digit = (uint32_t)(character - '0');
+                else if (character >= 'a' && character <= 'f')
+                    digit = (uint32_t)(character - 'a' + 10);
+                else
+                    digit = (uint32_t)(character - 'A' + 10);
+
+                number = number * 16 + digit;
+                has_digit = true;
+                character = scan_char();
+            }
+
+            if (!has_digit) {
+                scan_unread_char(character);
+                break;
+            }
+
+            scan_unread_char(character);
+            *value = number;
+            assigned++;
+            break;
+        }
+
+        default:
+            scan_unread_char('%');
+            va_end(args);
+            return assigned;
+        }
+
+        format++;
+    }
+
+    va_end(args);
+    return assigned;
+}
 
 void print(const char *str) {
     while (*str) {
